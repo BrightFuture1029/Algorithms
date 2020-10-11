515aab20b64da9343ac26856653ac31a68c2dfa6
dhirajdusane <dhirajdusane@hotmail.com>
2020-10-11 20:08:51 +0530

    few files

---
+++ ConsoleAlgorithms/ConsoleAlgorithms/Anagram.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleAlgorithms
+{
+    /// <summary>
+    /// Wiki:
+    /// An anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+    /// For example, the word anagram can be rearranged into nag a ram, or the word binary into brainy or the word adobe into abode.    /// </summary>
+    public class Anagram : IExecute
+    {
+        public void Execute(string[] args)
+        {
+            
+        }
+
+        public bool IsAnagrm(string lfs,string rhs)
+        {
+            var clhs = lfs.ToCharArray();
+            var crhs = rhs.ToCharArray();
+
+            int[] asciTable = new int[256];
+
+            for (int i = 0; i < clhs.Length; i++)
+            {
+
+            }
+
+            return true;
+        }
+    }
+}
---
+++ ConsoleAlgorithms/ConsoleAlgorithms/IExecute.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleAlgorithms
+{
+    public interface IExecute
+    {
+        public void Execute(string[] args);
+    }
+}
